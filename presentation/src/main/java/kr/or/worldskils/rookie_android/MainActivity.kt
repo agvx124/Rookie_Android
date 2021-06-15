ac672cea972fe9c67e2f53bff732e06629dddcc0
Dong_Hyeok_Na <43693001+agvx124@users.noreply.github.com>
2021-06-15 16:04:10 +0900

    Revert "[FIX] Get Sensor Data"
    This reverts commit 85861bcb97f4942c631e72899772cc6bdb6097f7.

--- presentation/src/main/java/kr/or/worldskils/rookie_android/MainActivity.kt
+++ presentation/src/main/java/kr/or/worldskils/rookie_android/MainActivity.kt
@@ -1,10 +1,9 @@
-package kr.or.worldskils.rookie_android.ui
+package kr.or.worldskils.rookie_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kr.or.worldskils.rookie_android.R
 
-class MainActivity : AppCompatActivity() {
+class   MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
