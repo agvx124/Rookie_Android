6871f1700a5ba365b4a969b6d1287666f10f5464
Dong_Hyeok_Na <43693001+agvx124@users.noreply.github.com>
2021-06-24 17:32:29 +0900

    [ADD] Remain Time

--- presentation/src/main/java/kr/or/worldskils/rookie_android/ui/MainActivity.kt
+++ presentation/src/main/java/kr/or/worldskils/rookie_android/ui/MainActivity.kt
@@ -1,21 +1,22 @@
 package kr.or.worldskils.rookie_android.ui
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.annotation.SuppressLint
+import android.os.Handler
+import android.os.Message
 import android.util.Log
-import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
-import android.widget.Toolbar
 import androidx.core.view.GravityCompat
 import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.material.navigation.NavigationView
 import com.google.firebase.messaging.FirebaseMessaging
 import kr.or.worldskils.rookie_android.R
 import kr.or.worldskils.rookie_android.base.BaseActivity
 import kr.or.worldskils.rookie_android.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.concurrent.thread
+
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
 
@@ -33,12 +34,26 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
     override fun setUp() {
         setFirebase()
         setActionBar()
+        binding.remainTextClock.text = getRemainTime()
 
         binding.navigationView.setNavigationItemSelectedListener { item ->
             item.isChecked = true
             binding.drawerLayout.closeDrawers()
 
             true
+        }
+
+        val handler = @SuppressLint("HandlerLeak")
+        object : Handler() {
+            override fun handleMessage(msg: Message) {
+                binding.remainTextClock.text = getRemainTime()
+            }
+        }
+        thread(start = true) {
+            while (true) {
+                Thread.sleep(1000)
+                handler.sendEmptyMessage(0)
+            }
         }
     }
 
@@ -77,5 +92,40 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
         supportActionBar!!.setDisplayShowTitleEnabled(false)
         supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_menu)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    // 월요일 10시부터 시작.
+    private fun getMonday(): Date {
+        val cal = Calendar.getInstance()
+
+        return cal.time
+    }
+
+    private fun getFriday(): Date {
+        val cal = Calendar.getInstance()
+        cal.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY)
+        cal.set(Calendar.HOUR_OF_DAY, 12)
+        cal.set(Calendar.MINUTE, 0)
+        cal.set(Calendar.SECOND, 0)
+
+        return cal.time
+    }
+
+    private fun getRemainTime(): String {
+        val remainTime = getFriday().time - getMonday().time
+        val diffSeconds = remainTime / 1000
+
+        val hour = diffSeconds / (60 * 60)
+        val minute = diffSeconds / 60 - hour * 60
+        val second = diffSeconds % 60
+
+        val day = (remainTime/ (24*60*60*1000)).toInt()
+
+
+        return if (day != 0) {
+            "$day Day$hour : $minute : $second"
+        } else {
+            "$hour : $minute : $second"
+        }
     }
 }
